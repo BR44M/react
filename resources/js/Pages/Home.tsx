539e075c692308a1a901d6dc307614b9cb54b25b
BR44M <braam1144@gmail.com>
2024-08-24 20:46:21 +0200

    [first_task] - added simple navbar for testing

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,7 +1,13 @@
-export default function Home(){
+import Layout from "../Layouts/Layout";
+
+function Home({ name }){
   return(
     <>
       <h1 className="title">Dit werk!</h1>
     </>
   );
 }
+
+Home.layout = page => <Layout children={page} />
+
+export default Home;
