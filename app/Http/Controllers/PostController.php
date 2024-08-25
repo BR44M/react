d8cf8277d9209b00ec91bb866d97b15f824ebce4
BR44M <braam1144@gmail.com>
2024-08-25 12:22:28 +0200

    [first_task] - pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::latest();
+        $posts = Post::latest()->paginate(5);
         return Inertia('Home', ['posts' => $posts]);
     }
 
--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -6,12 +6,31 @@ export default function Home({ posts }){
     <>
       <h1 className="title">List posts</h1>
       <div>
-        {posts.map(post => (
-            <li id={post.id} className="p-4 border-b">
+        {posts.data.map(post => (
+            <div id={post.id} className="p-4 border-b">
               <h3 className="title">{post.title}</h3>
               <span className="text-sm text-slate-600">Posts on: {new Date(post.created_at).toLocaleTimeString()}</span>
               <p className="font-medium">{post.body}</p>
-            </li>
+            </div>
+        ))}
+      </div>
+      <div className="py-12 px-4">
+        {posts.links.map(link => (
+          link.url ?
+          <Link
+            key={link.label}
+            href={link.url}
+            dangerouslySetInnerHTML={{ __html:link.label }}
+            className={`p-1 mx-1 ${
+              link.active ? "text-blue-500 font-bold" :""
+            }`}
+          />
+          :
+          <span
+            key={link.label}
+            dangerouslySetInnerHTML={{ __html:link.label }}
+            className="p-1 mx-1 text-slate-300"
+          ></span>
         ))}
       </div>
     </>
